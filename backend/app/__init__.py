437cca8de584fdb9ec3835d42284cc2f742a3353
Jeferson14 <je144cc@gmail.com>
2026-05-20 13:59:35 -0500

    HU-13 Recomendaciones personalizadas implementado

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -10,7 +10,7 @@ from app.models.usuario import db,bcrypt #Modelo de usuario
 from app.routes.auth_routes import auth_bp #Rutas de autenticación
 from dotenv import load_dotenv #Manejo de variables de entorno
 from flask_migrate import Migrate
-
+from app.routes.recomendaciones_routes import recomendaciones_bp
 from app.models.habito import Habito # Importar modelo para que SQLAlchemy/Migrate lo detecte
 from app.routes.habitos_routes import habitos_bp #Rutas de hábitos
 from app.models.evaluacion import EvaluacionRiesgo # Modelo Evaluacion de Riesgo
@@ -49,6 +49,7 @@ def crear_app():
     app.register_blueprint(auth_bp)
     app.register_blueprint(habitos_bp)
     app.register_blueprint(evaluaciones_bp)
+    app.register_blueprint(recomendaciones_bp)
 
     # Comando CLI para inicializar la base de datos manualmente
     @app.cli.command("init-db")
